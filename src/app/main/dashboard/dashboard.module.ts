e7c1a8045a2300e930c15f1c660e3d2c0c0602ae
Yim Klok <yimklok.kh@gmail.com>
2023-02-26 00:06:23 +0700

    dashboard component

--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ScrollbarModule } from 'helpers/directives/scrollbar';
+import { SharedModule } from 'app/shared/shared.module';
 
 const dashboardRoutes: Routes = [
   {
@@ -14,6 +16,8 @@ const dashboardRoutes: Routes = [
       DashboardComponent,
     ],
     imports: [
+      ScrollbarModule,
+      SharedModule,
       RouterModule.forChild(dashboardRoutes)
     ]
 })
