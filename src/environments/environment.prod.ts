65162ff06cfd1134bcef3eec3f65b1f6a4ef02e6
Meymey <meymeyheng2021@gmail.com>
2023-11-09 01:26:25 +0700

    new file environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
 
     production: true,
     apiUrl      : 'https://api.meymey.tech/api',
-    fileUrl     : 'http://161.97.136.126:8003/'
+    fileUrl     : 'http://file.meymey.tech/'
 
 };
