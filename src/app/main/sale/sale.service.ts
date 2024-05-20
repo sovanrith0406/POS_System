e83a53cc03ac4742b773b1f39c7ba6efc6f8a43b
panha <ngimpanha0.com>
2024-05-21 00:33:55 +0700

    Create Sale service file

---
+++ src/app/main/sale/sale.service.ts
@@ -0,0 +1,73 @@
+// ==========================================================>> Core Library
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// ==========================================================>> Custom Library
+import { environment as env } from 'environments/environment';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class SaleService {
+    public url: string = env.API_BASE_URL;
+    public httpOptions = {
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    };
+
+    constructor(private http: HttpClient) {}
+
+    /**
+     |-------------------------------------------------------------------
+     | Learn Read Delete (RD)
+     |-------------------------------------------------------------------
+     |
+     | develop by: Yim Klok
+     |
+     */
+    // ==================== Read All Products
+    getData(params = {}): any {
+        const httpOptions = {
+            headers: new HttpHeaders().set('Content-Type', 'application/json'),
+        };
+        httpOptions['params'] = params;
+        return this.http.get(this.url + '/admin/sales', httpOptions);
+    }
+    // ==================== Delete One Product
+    print(receipt_number: number = 0): any {
+        return this.http.get(
+            this.url + '/admin/sales/print/' + receipt_number,
+            this.httpOptions
+        );
+    }
+    // ==================== Delete One Product
+    delete(id: number = 0): any {
+        return this.http.delete(
+            this.url + '/admin/sales/' + id,
+            this.httpOptions
+        );
+    }
+    //==================================================================
+
+    //// =================================>> Convert base64 to blob
+    b64toBlob(b64Data: any, contentType: any, sliceSize: any) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+        for (
+            var offset = 0;
+            offset < byteCharacters.length;
+            offset += sliceSize
+        ) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+            var byteArray = new Uint8Array(byteNumbers);
+            byteArrays.push(byteArray);
+        }
+        var blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+    }
+}
