b33ab73d21d8e89c93d1e2294256bde56ee0a87a
panha <ngimpanha0.com>
2024-05-21 00:32:29 +0700

    Create Sale module file

---
+++ src/app/main/sale/sale.module.ts
@@ -0,0 +1,23 @@
+// ==========================================================>> Core Library
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// ==========================================================>> Custom Library
+import { SharedModule } from 'app/shared/shared.module';
+import { ScrollbarModule } from 'helpers/directives/scrollbar';
+
+import { ListingComponent } from './listing/listing.component';
+import { DetailsComponent } from './view-detail-dialog/details.component';
+
+const saleRoutes: Routes = [
+    {
+        path: 'xyz',
+        component: ListingComponent,
+    },
+];
+
+@NgModule({
+    imports: [SharedModule, RouterModule.forChild(saleRoutes), ScrollbarModule],
+    declarations: [ListingComponent, DetailsComponent],
+})
+export class SaleModule {}
