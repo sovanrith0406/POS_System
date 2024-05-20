5bc3c1f2a61ad42053815c2ab0be81afa53cddb0
panha <ngimpanha0.com>
2024-05-21 01:32:30 +0700

    Create listing component

---
+++ src/app/main/my-profile/listing/my-profile.component.ts
@@ -0,0 +1,13 @@
+// ==========================================================>> Core Library
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-my-profile',
+    templateUrl: './my-profile.component.html',
+    styleUrls: ['./my-profile.component.scss'],
+})
+export class MyProfileComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
