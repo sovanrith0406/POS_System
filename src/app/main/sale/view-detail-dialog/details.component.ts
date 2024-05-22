48ff920817698f575a6a6c91f9867e1eb79fc5e6
panha <ngimpanha0.com>
2024-05-23 00:00:50 +0700

    fix web

--- src/app/main/sale/view-detail-dialog/details.component.ts
+++ src/app/main/sale/view-detail-dialog/details.component.ts
@@ -64,21 +64,21 @@ export class DetailsComponent implements OnInit {
     }
 
     // =================================>> Convert base64 to blob
-    // b64toBlob(b64Data: any, contentType: any, sliceSize: any) {
-    //   contentType = contentType || '';
-    //   sliceSize = sliceSize || 512;
-    //   var byteCharacters = atob(b64Data);
-    //   var byteArrays = [];
-    //   for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    //     var slice = byteCharacters.slice(offset, offset + sliceSize);
-    //     var byteNumbers = new Array(slice.length);
-    //     for (var i = 0; i < slice.length; i++) {
-    //       byteNumbers[i] = slice.charCodeAt(i);
-    //     }
-    //     var byteArray = new Uint8Array(byteNumbers);
-    //     byteArrays.push(byteArray);
-    //   }
-    //   var blob = new Blob(byteArrays, { type: contentType });
-    //   return blob;
-    // }
+    b64toBlob(b64Data: any, contentType: any, sliceSize: any) {
+      contentType = contentType || '';
+      sliceSize = sliceSize || 512;
+      var byteCharacters = atob(b64Data);
+      var byteArrays = [];
+      for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+        }
+        var byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+      }
+      var blob = new Blob(byteArrays, { type: contentType });
+      return blob;
+    }
 }
