cbdefd4609916df01227dfa9922bdbdbf78b5eb6
panha <ngimpanha0.com>
2024-05-28 18:30:54 +0700

    fix web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
-
-    production: true,
-    API_BASE_URL      : 'http://localhost:8002/api',
-    FILE_PUBLIC_BASE_URL     : 'http://localhost:8003/'
-
+    production: false,
+    API_BASE_URL: 'https://api.ngimpanha.cloud/api',
+    FILE_PUBLIC_BASE_URL: 'https://file.ngimpanha.cloud/',
 };
