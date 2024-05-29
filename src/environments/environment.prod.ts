660cea644272c83b71f885418c3724ecdaec277d
panha <ngimpanha0.com>
2024-05-29 20:47:00 +0700

    fix hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_BASE_URL: 'http://146.190.106.110:5002/api',
-    FILE_PUBLIC_BASE_URL: 'http://146.190.106.110:5003/',
+    API_BASE_URL: 'https://api.ngimpanha.cloud/api',
+    FILE_PUBLIC_BASE_URL: 'https://file.ngimpanha.cloud/',
 };
